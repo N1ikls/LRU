2a0521926470208eec4722a048e46fcd86e4fa8c
Nik <sau3000@mail.ru>
2021-12-09 13:19:42 +0300

    edit fix

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 import LRU from "./LRU.js";
+(function () {
+  const a = new LRU(4);
+  a.put(1, 1);
+  a.put(2, 1);
+  a.get(2);
+  a.put(3, 1);
+  a.put(4, 1);
+  a.put(5, 1);
+  console.log(a.mas);
+})();
+(function () {
+  const a = new LRU(4);
+  a.put(1, 1);
+  a.put(2, 1);
+  a.get(2);
+  a.put(3, 1);
+  a.put(4, 1);
+  a.put(5, 1);
+  console.log(a.mas);
+})();
+
 //Testing
-const cache = new LRU(4);
-// in 1,2 -- 6,5,4,3
-cache.put(1, 1);
-cache.put(2, 1);
-cache.get(2);
-cache.put(3, 1);
-cache.put(4, 1);
-cache.put(5, 1);
-cache.put(6, 1);
-//3,4,5,6 out
-console.log(cache.mas);
+// const cache = new LRU(4);
+// // in 1,2 -- 6,5,4,3
+// cache.put(1, 1);
+// cache.put(2, 1);
+// cache.get(2);
+// cache.put(3, 1);
+// cache.put(4, 1);
+// cache.put(5, 1);
+// cache.put(6, 1);
+// //3,4,5,6 out
+// console.log(cache.mas);
