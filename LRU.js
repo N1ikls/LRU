41fcc2130c8b8181aa2e74e5705bbfea135fb1a4
Nik <sau3000@mail.ru>
2021-12-10 11:09:49 +0300

    fix LRU

--- LRU.js
+++ LRU.js
@@ -1,29 +1,32 @@
-export default class LRU {
-  constructor(len) {
-    this.len = len;
-    this.mas = [];
-    this.map = {};
-  }
-  get(key) {
-    if (!this.map[key]) {
-      return -1;
+export default (function () {
+  class LRU {
+    constructor(len) {
+      this.len = len;
+      this.mas = [];
+      this.map = {};
     }
-    const value = this.map[key].value;
-    this.put(key, value);
-    return value;
-  }
-  put(key, value) {
-    if (this.map[key]) {
-      this.mas.splice(
-        this.mas.findIndex((i) => i === this.map[key]),
-        1
-      );
-      this.map[key] = null;
+    get(key) {
+      if (!this.map[key]) {
+        return -1;
+      }
+      const value = this.map[key].value;
+      this.put(key, value);
+      return value;
     }
-    this.mas.push({ key, value });
-    this.map[key] = this.mas[this.mas.length - 1];
-    if (this.mas.length > this.len) {
-      this.map[this.mas.shift().key] = null;
+    put(key, value) {
+      if (this.map[key]) {
+        this.mas.splice(
+          this.mas.findIndex((i) => i === this.map[key]),
+          1
+        );
+        this.map[key] = null;
+      }
+      this.mas.push({ key, value });
+      this.map[key] = this.mas[this.mas.length - 1];
+      if (this.mas.length > this.len) {
+        this.map[this.mas.shift().key] = null; // удаляем если у нас перебор по ключу значения
+      }
     }
   }
-}
+  return LRU;
+})();
--- main.js
+++ main.js
@@ -1,25 +1,14 @@
 import LRU from "./LRU.js";
-(function () {
-  const a = new LRU(4);
-  a.put(1, 1);
-  a.put(2, 1);
-  a.get(2);
-  a.put(3, 1);
-  a.put(4, 1);
-  a.put(5, 1);
-  console.log(a.mas);
-})();
-(function () {
-  const a = new LRU(4);
-  a.put(1, 1);
-  a.put(2, 1);
-  a.get(2);
-  a.put(3, 1);
-  a.put(4, 1);
-  a.put(5, 1);
-  console.log(a.mas);
-})();
-
+const a = new LRU(4);
+a.put(1, 1);
+a.put(2, 1);
+a.get(2);
+a.put(3, 1);
+a.put(4, 1);
+a.put(5, 1);
+a.put(6, 1);
+a.get(6);
+console.log(a.mas);
 //Testing
 // const cache = new LRU(4);
 // // in 1,2 -- 6,5,4,3
